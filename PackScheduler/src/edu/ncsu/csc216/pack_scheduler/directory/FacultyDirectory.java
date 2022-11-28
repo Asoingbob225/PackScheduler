193f3cfa23b09c3c1c8e31841ecd9d52f6b53114
rdbryan2 <rdbryan2@ncsu.edu>
2022-11-27 21:34:19 -0500

    [Debugging] Fixed a bug

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/FacultyDirectory.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/FacultyDirectory.java
@@ -95,7 +95,7 @@ public class FacultyDirectory {
 
 		// If an IllegalArgumentException is thrown, it's passed up from Faculty
 		// to the GUI
-		Faculty f = new Faculty(firstName, lastName, id, email, password, maxCourses);
+		Faculty f = new Faculty(firstName, lastName, id, email, hashPW, maxCourses);
 
 		for (int i = 0; i < facultyDirectory.size(); i++) {
 			User s = facultyDirectory.get(i);
